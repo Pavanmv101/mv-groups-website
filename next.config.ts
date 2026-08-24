8bc6f62c12f1a3f0db72e443feb9f65b4f1ed0a6
Pavan MV <pavanmv101gmail@example.com>
2026-08-25 00:51:07 +0530

    fix: Remove redirect loop and unused js scripts causing lint errors

--- next.config.ts
+++ next.config.ts
@@ -15,16 +15,6 @@ const nextConfig: NextConfig = {
       },
     ]
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [{ type: 'host', value: 'www.mvgroups.online' }],
-        destination: 'https://mvgroups.online/:path*',
-        permanent: true,
-      },
-    ]
-  },
   images: {
     remotePatterns: [
       {
